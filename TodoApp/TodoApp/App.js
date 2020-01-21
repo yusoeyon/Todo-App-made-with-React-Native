c8e20e8614c075ea77825ff778cc5f3565447d41
yusoyeon <ysy020516@dgsw.hs.kr>
2020-01-21 16:20:04 +0900

    Ui part one

---
+++ TodoApp/TodoApp/App.js
@@ -0,0 +1,54 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+
+const { height, width } = Dimensions.get("window");
+
+export default class App extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>To Do</Text>
+        <View style={styles.card}>
+          <TextInput stule={styles.input} placeholder={"New To Do"} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F23657",
+    alignItems: 'center',
+  },
+  title: {
+    color: "white",
+    fontSize: 35,
+    marginTop: 80,
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card:{
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios:{
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android:{
+        elevation: 3
+      }
+    })
+  }
+});
